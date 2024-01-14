0bf7ced57b67c7d357a6183cb4bb16181c831e96
dlara <tutoriasklen@outlook.com>
2024-01-13 22:12:41 -0500

    add js

---
+++ js/mokepon.js
@@ -0,0 +1,6 @@
+function seleccionarMacotaJugador() {
+    alert('SELECCIONASTE TU MASCOTA')
+}
+
+let botonMascotaJugador = document.getElementById('boton-mascota')
+botonMascotaJugador.addEventListener('click', seleccionarMacotaJugador)
