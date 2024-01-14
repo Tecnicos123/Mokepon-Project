4536d30f15e4e2fc4d33ad5aba0a240dc138685c
dlara <tutoriasklen@outlook.com>
2024-01-14 16:29:09 -0500

    add js 2

--- js/mokepon.js
+++ js/mokepon.js
@@ -1,6 +1,12 @@
-function seleccionarMacotaJugador() {
-    alert('SELECCIONASTE TU MASCOTA')
+function iniciarJuego() {
+    let botonMascotaJugador = document.getElementById('boton-mascota')
+    botonMascotaJugador.addEventListener('click', seleccionarMacotaJugador)
 }
 
-let botonMascotaJugador = document.getElementById('boton-mascota')
-botonMascotaJugador.addEventListener('click', seleccionarMacotaJugador)
+function seleccionarMacotaJugador() {
+    if (document.getElementById('hipodogue'),checked) {
+        alert('Seleccionaste a Hipodogue')
+    }
+}
+
+windows.addEventListener('load', iniciarJuego)
