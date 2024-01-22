e2bcfed2ddd5fc237d32017c4c52ce1e5b796b0c
Tecnicos TK <152911340+Tecnicos123@users.noreply.github.com>
2024-01-21 20:09:53 -0500

    "clase 22"

--- js/mokepon.js
+++ js/mokepon.js
@@ -4,9 +4,35 @@ function iniciarJuego() {
 }
 
 function seleccionarMacotaJugador() {
-    if (document.getElementById('hipodogue'),checked) {
-        alert('Seleccionaste a Hipodogue')
+    let inputHipodogue = document.getElementById('hipodogue')
+    let inputCapipepo = document.getElementById('capipepo')
+    let inputRatigueya = document.getElementById('ratigueya')
+    let spanMascotaJugador = document.getElementById('mascota-jugador')
+
+    if (inputHipodogue.checked) {
+        spanMascotaJugador.innerHTML = 'hipodogue'
+    } else if (inputCapipepo.checked) {
+        spanMascotaJugador.innerHTML = 'capipepo'
+    } else if (inputRatigueya.checked) {
+        spanMascotaJugador.innerHTML = 'ratigueya'
+    } else {
+        alert('Selecciona una mascota')
     }
+
+    seleccionarMacotaEnemigo()
+
+}
+
+function seleccionarMacotaEnemigo() {
+    let ataqueAleatorio = aleatorio(1,3)
+
+    if (ataqueAleatorio ==1) {
+        
+    }
+}
+
+function aleatorio(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
 windows.addEventListener('load', iniciarJuego)
